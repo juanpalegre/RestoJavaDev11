79bdaf22feeb9bcc063d7076c65763feb79566ee
Juan Alegre <jpalegree@gmail.com>
2023-04-27 23:31:50 -0300

    Initial commit

---
+++ src/test/java/com/example/restojavadev11/RestoJavaDev11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restojavadev11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestoJavaDev11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
